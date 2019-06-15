69688f0ff6631e6ed58d94cced75326574189a4e
Aloys Baillet <aloys.baillet@gmail.com>
2019-06-15 17:51:19 +1000

    Added clang
    Signed-off-by: Aloys Baillet <aloys.baillet@gmail.com>

---
+++ scripts/install_clang.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -ex
+
+CLANG_VERSION="$1"
+
+git clone https://github.com/llvm/llvm-project.git
+cd llvm-project
+
+if [ "$CLANG_VERSION" != "latest" ]; then
+    git checkout tags/llvmorg-${CLANG_VERSION} -b llvmorg-${CLANG_VERSION}
+fi
+
+mkdir build
+cd build
+cmake -DLLVM_ENABLE_PROJECTS=clang -G "Unix Makefiles" ../llvm
+make
+make install
+
+cd ../..
+rm -rf llvm-project
