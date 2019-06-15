397ad253729e3b3295c088b23e6b39558b3941ca
Aloys Baillet <aloys.baillet@gmail.com>
2019-06-15 16:26:22 +1000

    PR feedback with added ci-openexr
    Signed-off-by: Aloys Baillet <aloys.baillet@gmail.com>

--- scripts/install_ninja.sh
+++ scripts/install_ninja.sh
@@ -3,10 +3,6 @@
 set -ex
 
 NINJA_VERSION="$1"
-NINJA_MAJOR_MINOR=$(echo "${NINJA_VERSION}" | cut -d. -f-2)
-NINJA_MAJOR=$(echo "${NINJA_VERSION}" | cut -d. -f-1)
-NINJA_MINOR=$(echo "${NINJA_MAJOR_MINOR}" | cut -d. -f2-)
-NINJA_PATCH=$(echo "${NINJA_VERSION}" | cut -d. -f3-)
 
 wget https://github.com/ninja-build/ninja/releases/download/v${NINJA_VERSION}/ninja-linux.zip -O /tmp/ninja.zip
 
