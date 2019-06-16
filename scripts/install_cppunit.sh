729c22633d34904fbc7d20e554735d153f0458eb
Aloys Baillet <aloys.baillet@gmail.com>
2019-06-16 14:06:40 +1000

    Cleanup and added cppunit
    Signed-off-by: Aloys Baillet <aloys.baillet@gmail.com>

---
+++ scripts/install_cppunit.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -ex
+
+CPPUNIT_VERSION="$1"
+
+wget http://dev-www.libreoffice.org/src/cppunit-${CPPUNIT_VERSION}.tar.gz
+tar xf cppunit-${CPPUNIT_VERSION}.tar.gz
+cd cppunit-${CPPUNIT_VERSION}
+
+./autogen.sh
+./configure --prefix=/usr/local
+make -j4
+make install
+
+cd ..
+rm -rf cppunit-${CPPUNIT_VERSION}
