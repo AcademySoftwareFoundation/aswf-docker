64bd562d4ceefd63d2d5b637152e66559d887044
eoinm <eoinm@al.com.au>
2020-02-07 15:33:51 +1100

    patch to fix mayapy crash in devtoolset6

--- scripts/vfx/build_usd.sh
+++ scripts/vfx/build_usd.sh
@@ -8,7 +8,37 @@ if [ ! -f $DOWNLOADS_DIR/usd-${USD_VERSION}.tar.gz ]; then
 fi
 
 tar -zxf $DOWNLOADS_DIR/usd-${USD_VERSION}.tar.gz
+
+touch $DOWNLOADS_DIR/USD-${USD_VERSION}/pxr/base/lib/vt/devtoolset6Workaround.cpp
+echo '#if (__GNUC__ >= 6)
+#include <cstdlib>#pragma weak __cxa_throw_bad_array_new_lengthextern "C" void
+__cxa_throw_bad_array_new_length()
+{
+ abort();
+}
+#endif' >> $DOWNLOADS_DIR/USD-${USD_VERSION}/pxr/base/lib/vt/devtoolset6Workaround.cpp
 cd USD-${USD_VERSION}
+cat <<EOF | patch -p1
+diff --git a/pxr/base/lib/vt/CMakeLists.txt b/pxr/base/lib/vt/CMakeLists.txt
+index aecffd7fb..c8f840bed 100644
+--- a/pxr/base/lib/vt/CMakeLists.txt
++++ b/pxr/base/lib/vt/CMakeLists.txt
+@@ -38,6 +38,9 @@ pxr_library(vt
+ 
+     PRIVATE_HEADERS
+         typeHeaders.h
++        
++    CPPFILES
++        devtoolset6Workaround.cpp
+ 
+     PYTHON_CPPFILES
+         moduleDeps.cpp
+EOF
+
+
+
+
+
 
 mkdir build
 cd build
