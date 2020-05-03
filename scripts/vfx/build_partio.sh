30152da38bffb8de1470c02a02dfd09ab1186e8f
Aloys Baillet <aloys.baillet+github@gmail.com>
2020-05-03 15:01:01 +1000

    Added partio package for osl and vfxall
    Signed-off-by: Aloys Baillet <aloys.baillet+github@gmail.com>

---
+++ scripts/vfx/build_partio.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# Copyright (c) Contributors to the aswf-docker Project. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+set -ex
+
+if [ ! -f $DOWNLOADS_DIR/partio-${PARTIO_VERSION}.tar.gz ]; then
+    curl --location https://github.com/wdas/partio/archive/v${PARTIO_VERSION}.tar.gz -o $DOWNLOADS_DIR/partio-${PARTIO_VERSION}.tar.gz
+fi
+
+tar -zxf $DOWNLOADS_DIR/partio-${PARTIO_VERSION}.tar.gz
+cd partio-${PARTIO_VERSION}
+
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=${ASWF_INSTALL_PREFIX} \
+      ..
+make -j$(nproc)
+make install
+
+cd ../..
+rm -rf partio-${PARTIO_VERSION}
