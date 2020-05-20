e928b09b9e0fbceba28cef7e5867cdf0d78dc84b
Aloys Baillet <aloys.baillet+github@gmail.com>
2020-05-20 23:36:19 +1000

    Added github access token settings
    Signed-off-by: Aloys Baillet <aloys.baillet+github@gmail.com>

---
+++ python/aswfdocker/tests/test_settings.py
@@ -0,0 +1,39 @@
+# Copyright (c) Contributors to the aswf-docker Project. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+"""
+Tests for user settings
+"""
+
+import unittest
+import tempfile
+import os
+import logging
+
+from click.testing import CliRunner
+
+from aswfdocker import settings
+from aswfdocker.cli import aswfdocker
+
+
+class TestSettingsCli(unittest.TestCase):
+    def setUp(self):
+        logging.getLogger("").handlers = []
+
+    def test_settings_cli(self):
+        runner = CliRunner()
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            settings_path = os.path.join(tmpdirname, "tmpsettings.yaml")
+            result = runner.invoke(
+                aswfdocker.cli,
+                [
+                    "settings",
+                    "--github-access-token",
+                    "foobar",
+                    "--settings-path",
+                    settings_path,
+                ],
+            )
+            self.assertEqual(result.output, "")
+            self.assertEqual(result.exit_code, 0)
+            s = settings.Settings(settings_path=settings_path)
+            self.assertEqual(s.github_access_token, "foobar")
