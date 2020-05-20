bc130f7ab164a4c0b092ed592fa28768e5dd9bc8
Aloys Baillet <aloys.baillet+github@gmail.com>
2020-05-20 23:36:19 +1000

    Added github access token settings
    Signed-off-by: Aloys Baillet <aloys.baillet+github@gmail.com>
    Signed-off-by: Miles Lauridsen <mileslauridsen@gmail.com>
    Signed-off-by: Miles Lauridsen <mileslauridsen@protonmail.com>

---
+++ python/aswfdocker/settings.py
@@ -0,0 +1,23 @@
+# Copyright (c) Contributors to the aswf-docker Project. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+from yaml import load, dump
+
+
+class Settings:
+    def __init__(self, settings_path="~/.aswfdocker"):
+        self.settings_path = os.path.expanduser(settings_path)
+        self.github_access_token = ""
+        self.load()
+
+    def load(self):
+        if os.path.exists(self.settings_path):
+            with open(self.settings_path) as f:
+                data = load(f)
+                self.github_access_token = data.get("github_access_token", "")
+
+    def save(self):
+        data = {"github_access_token": self.github_access_token}
+        with open(self.settings_path, "w") as f:
+            dump(data, f)
