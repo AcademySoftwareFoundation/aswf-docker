0a1d92f47b8e81d3de2c21c67c53a24ae92036d5
Aloys Baillet <aloys.baillet+github@gmail.com>
2020-05-28 18:27:02 +1000

    Fixing 2021
    Signed-off-by: Aloys Baillet <aloys.baillet+github@gmail.com>

---
+++ scripts/tests/2/test_common.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# Copyright (c) Contributors to the aswf-docker Project. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+set -ex
+
+clang_v=`clang --version`
+if [[ $clang_v != clang\ version\ 10.0* ]]
+then
+    exit 1
+fi
+
+gcc_v=`gcc --version`
+if [[ $gcc_v != gcc\ \(GCC\)\ 9.1* ]]
+then
+    exit 1
+fi
+
+ninja_v=`ninja --version`
+if [[ $ninja_v != 1.* ]]
+then
+    exit 1
+fi
