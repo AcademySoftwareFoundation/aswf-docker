ee9e4ea8d1e6cafae9f1527d09b9c7c842181fca
Aloys Baillet <aloys.baillet+github@gmail.com>
2020-08-01 19:17:20 +1000

    Added ci-otio and added otio into ci-vfxall
    Signed-off-by: Aloys Baillet <aloys.baillet+github@gmail.com>

---
+++ scripts/vfx/build_otio.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Copyright (c) Contributors to the aswf-docker Project. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+set -ex
+
+git clone https://github.com/PixarAnimationStudios/OpenTimelineIO.git
+cd OpenTimelineIO
+
+if [ "$OTIO_VERSION" != "latest" ]; then
+    git checkout tags/v${OTIO_VERSION} -b v${OTIO_VERSION}
+fi
+
+pip install --prefix=${ASWF_INSTALL_PREFIX} .
+
+cd ..
+rm -rf OpenTimelineIO
