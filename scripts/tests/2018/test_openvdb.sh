2b28c62d946e194141091a17c1f0d20f201d38a5
Simran Spiller <simran.spiller@gmail.com>
2020-09-21 01:32:27 +0200

    Whitespace
    Signed-off-by: Simran Spiller <simran.spiller@gmail.com>

--- scripts/tests/2018/test_openvdb.sh
+++ scripts/tests/2018/test_openvdb.sh
@@ -4,7 +4,7 @@
 
 set -ex
 
-git clone https://github.com/AcademySoftwareFoundation/openvdb 
+git clone https://github.com/AcademySoftwareFoundation/openvdb
 cd openvdb
 
 git checkout b5af82007f869a29f6fa1af729f14763e99f85be # Known working at that commit...
