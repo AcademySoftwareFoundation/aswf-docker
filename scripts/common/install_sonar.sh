657f81bf163d3f58a084dd1a4f0792c725eaa7dc
Simran Spiller <simran.spiller@gmail.com>
2020-09-22 00:12:12 +0200

    Remove useless echo
    Signed-off-by: Simran Spiller <simran.spiller@gmail.com>

--- scripts/common/install_sonar.sh
+++ scripts/common/install_sonar.sh
@@ -13,7 +13,7 @@ fi
 unzip "$DOWNLOADS_DIR/sonar-bw.zip"
 mv build-wrapper-linux-x86 /var/opt/.
 ln -s /var/opt/build-wrapper-linux-x86/build-wrapper-linux-x86-64 /usr/bin/build-wrapper-linux-x86-64
-echo $(build-wrapper-linux-x86-64 --help)
+build-wrapper-linux-x86-64 --help
 
 if [ ! -f "$DOWNLOADS_DIR/sonar-scanner.zip" ]; then
     curl --location "https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-${SONAR_VERSION}-linux.zip" -o "$DOWNLOADS_DIR/sonar-scanner.zip"
@@ -21,7 +21,7 @@ fi
 unzip "$DOWNLOADS_DIR/sonar-scanner.zip"
 mv "sonar-scanner-${SONAR_VERSION}-linux" /var/opt/.
 ln -s "/var/opt/sonar-scanner-${SONAR_VERSION}-linux/bin/sonar-scanner" /usr/bin/sonar-scanner
-echo $(sonar-scanner --help)
+sonar-scanner --help
 
 cd ..
 rm -rf sonar
