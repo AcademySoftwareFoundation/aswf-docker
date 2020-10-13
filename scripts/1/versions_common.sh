f968062ba3579a747464328d6a8ec5d17f9006f8
Aloys Baillet <aloys.baillet+github@gmail.com>
2020-10-14 06:40:01 +1100

    Updated clang7 from clang-7.0.1 to clang-7.1.0
    Signed-off-by: Aloys Baillet <aloys.baillet+github@gmail.com>

--- scripts/1/versions_common.sh
+++ scripts/1/versions_common.sh
@@ -6,7 +6,7 @@ set -ex
 
 export DTS_VERSION=6
 export CCACHE_VERSION=3.7.4
-export CLANG_VERSION=7.0.1
+export CLANG_VERSION=7.1.0
 export NINJA_VERSION=1.10.0
 export SONAR_VERSION=3.3.0.1492
 export PKGS_COMMON_CMAKE_VERSION=3.12.4 # Only used to build common packages
@@ -14,7 +14,7 @@ export PKGS_COMMON_CMAKE_VERSION=3.12.4 # Only used to build common packages
 if [[ $CLANG_MAJOR_VERSION == 6 ]]; then
     export CLANG_VERSION=6.0.1
 elif [[ $CLANG_MAJOR_VERSION == 7 ]]; then
-    export CLANG_VERSION=7.0.1
+    export CLANG_VERSION=7.1.0
 elif [[ $CLANG_MAJOR_VERSION == 8 ]]; then
     export CLANG_VERSION=8.0.1
 elif [[ $CLANG_MAJOR_VERSION == 9 ]]; then
