bab0df9797575241182fc7a9fa43b7c28082782a
Aloys Baillet <aloys.baillet+github@gmail.com>
2020-10-31 16:08:31 +1100

    Exposing package versions to docker images
    Signed-off-by: Aloys Baillet <aloys.baillet+github@gmail.com>

--- python/aswfdocker/tests/test_builder.py
+++ python/aswfdocker/tests/test_builder.py
@@ -34,39 +34,18 @@ class TestBuilder(unittest.TestCase):
         qt_version = list(
             index.Index().iter_versions(constants.ImageType.PACKAGE, "qt")
         )[1]
+        baked = b.make_bake_dict()
         self.assertEqual(
-            b.make_bake_dict(),
-            {
-                "group": {"default": {"targets": ["ci-package-qt-2019"]}},
-                "target": {
-                    "ci-package-qt-2019": {
-                        "context": ".",
-                        "dockerfile": "packages/Dockerfile",
-                        "args": {
-                            "ASWF_ORG": "aswflocaltesting",
-                            "ASWF_PKG_ORG": "aswftesting",
-                            "ASWF_VERSION": qt_version,
-                            "CI_COMMON_VERSION": "1",
-                            "CLANG_MAJOR_VERSION": "7",
-                            "CUDA_VERSION": "10.2",
-                            "DTS_VERSION": "6",
-                            "PYTHON_VERSION": "2.7",
-                            "VFXPLATFORM_VERSION": "2019",
-                        },
-                        "labels": {
-                            "org.opencontainers.image.created": constants.DEV_BUILD_DATE,
-                            "org.opencontainers.image.revision": constants.DEV_BUILD_DATE,
-                        },
-                        "tags": [
-                            f"{constants.DOCKER_REGISTRY}/aswflocaltesting/ci-package-qt:2019",
-                            f"{constants.DOCKER_REGISTRY}/aswflocaltesting/ci-package-qt:{qt_version}",
-                            f"{constants.DOCKER_REGISTRY}/aswflocaltesting/ci-package-qt:latest",
-                        ],
-                        "target": "ci-package-qt",
-                        "output": ["type=docker"],
-                    }
-                },
-            },
+            baked["target"]["ci-package-qt-2019"]["tags"],
+            [
+                f"{constants.DOCKER_REGISTRY}/aswflocaltesting/ci-package-qt:2019",
+                f"{constants.DOCKER_REGISTRY}/aswflocaltesting/ci-package-qt:{qt_version}",
+                f"{constants.DOCKER_REGISTRY}/aswflocaltesting/ci-package-qt:latest",
+                f"{constants.DOCKER_REGISTRY}/aswflocaltesting/ci-package-qt:2019-5.12.6",
+            ],
+        )
+        self.assertEqual(
+            baked["target"]["ci-package-qt-2019"]["args"]["ASWF_VERSION"], qt_version
         )
 
     def test_image_base_2019_dict(self):
@@ -82,38 +61,17 @@ class TestBuilder(unittest.TestCase):
         base_version = list(
             index.Index().iter_versions(constants.ImageType.CI_IMAGE, "base")
         )[1]
+        baked = b.make_bake_dict()
         self.assertEqual(
-            b.make_bake_dict(),
-            {
-                "group": {"default": {"targets": ["ci-base-2019"]}},
-                "target": {
-                    "ci-base-2019": {
-                        "context": ".",
-                        "dockerfile": "ci-base/Dockerfile",
-                        "args": {
-                            "ASWF_ORG": "aswflocaltesting",
-                            "ASWF_PKG_ORG": "aswftesting",
-                            "ASWF_VERSION": base_version,
-                            "CI_COMMON_VERSION": "1",
-                            "CLANG_MAJOR_VERSION": "7",
-                            "CUDA_VERSION": "10.2",
-                            "DTS_VERSION": "6",
-                            "PYTHON_VERSION": "2.7",
-                            "VFXPLATFORM_VERSION": "2019",
-                        },
-                        "labels": {
-                            "org.opencontainers.image.created": constants.DEV_BUILD_DATE,
-                            "org.opencontainers.image.revision": constants.DEV_BUILD_DATE,
-                        },
-                        "tags": [
-                            f"{constants.DOCKER_REGISTRY}/aswflocaltesting/ci-base:2019",
-                            f"{constants.DOCKER_REGISTRY}/aswflocaltesting/ci-base:{base_version}",
-                            f"{constants.DOCKER_REGISTRY}/aswflocaltesting/ci-base:latest",
-                        ],
-                        "output": ["type=docker"],
-                    }
-                },
-            },
+            baked["target"]["ci-base-2019"]["tags"],
+            [
+                f"{constants.DOCKER_REGISTRY}/aswflocaltesting/ci-base:2019",
+                f"{constants.DOCKER_REGISTRY}/aswflocaltesting/ci-base:{base_version}",
+                f"{constants.DOCKER_REGISTRY}/aswflocaltesting/ci-base:latest",
+            ],
+        )
+        self.assertEqual(
+            baked["target"]["ci-base-2019"]["args"]["ASWF_VERSION"], base_version
         )
 
     def test_image_base_2019clang_dict(self):
@@ -138,14 +96,43 @@ class TestBuilder(unittest.TestCase):
                         "context": ".",
                         "dockerfile": "ci-openvdb/Dockerfile",
                         "args": {
+                            "ALEMBIC_VERSION": "1.7.11",
                             "ASWF_ORG": "aswflocaltesting",
                             "ASWF_PKG_ORG": "aswftesting",
                             "ASWF_VERSION": openvdb_version,
+                            "BLOSC_VERSION": "1.5.0",
+                            "BOOST_VERSION": "1.66.0",
+                            "CCACHE_VERSION": "4.0",
                             "CI_COMMON_VERSION": "1",
                             "CLANG_MAJOR_VERSION": "9",
+                            "CLANG_VERSION": "9.0.1",
+                            "CMAKE_VERSION": "3.12.4",
+                            "CPPUNIT_VERSION": "1.14.0",
                             "CUDA_VERSION": "10.2",
                             "DTS_VERSION": "6",
-                            "PYTHON_VERSION": "2.7",
+                            "GLEW_VERSION": "2.1.0",
+                            "GLFW_VERSION": "3.1.2",
+                            "HDF5_VERSION": "1.8.21",
+                            "LOG4CPLUS_VERSION": "1.1.2",
+                            "NINJA_VERSION": "1.10.0",
+                            "NUMPY_VERSION": "1.14",
+                            "OCIO_CONFIGS_VERSION": "1.0_r2",
+                            "OCIO_VERSION": "1.1.0",
+                            "OIIO_VERSION": "2.0.8",
+                            "OPENEXR_VERSION": "2.3.0",
+                            "OPENSUBDIV_VERSION": "3_3_3",
+                            "OPENVDB_VERSION": "6.2.1",
+                            "OSL_VERSION": "1.10.9",
+                            "OTIO_VERSION": "0.12.1",
+                            "PARTIO_VERSION": "1.10.1",
+                            "PKGS_COMMON_CMAKE_VERSION": "3.12.4",
+                            "PTEX_VERSION": "2.1.33",
+                            "PYSIDE_VERSION": "5.12.6",
+                            "PYTHON_VERSION": "2.7.15",
+                            "QT_VERSION": "5.12.6",
+                            "SONAR_VERSION": "3.3.0.1492",
+                            "TBB_VERSION": "2018",
+                            "USD_VERSION": "19.11",
                             "VFXPLATFORM_VERSION": "2019",
                         },
                         "labels": {
@@ -184,14 +171,43 @@ class TestBuilder(unittest.TestCase):
                         "context": ".",
                         "dockerfile": "ci-base/Dockerfile",
                         "args": {
+                            "ALEMBIC_VERSION": "1.7.12",
                             "ASWF_ORG": "aswflocaltesting",
                             "ASWF_PKG_ORG": "aswftesting",
                             "ASWF_VERSION": base_versions[2],
+                            "BLOSC_VERSION": "1.5.0",
+                            "BOOST_VERSION": "1.70.0",
+                            "CCACHE_VERSION": "4.0",
                             "CI_COMMON_VERSION": "1",
                             "CLANG_MAJOR_VERSION": "7",
+                            "CLANG_VERSION": "7.1.0",
+                            "CMAKE_VERSION": "3.18.4",
+                            "CPPUNIT_VERSION": "1.14.0",
                             "CUDA_VERSION": "10.2",
                             "DTS_VERSION": "6",
-                            "PYTHON_VERSION": "3.7",
+                            "GLEW_VERSION": "2.1.0",
+                            "GLFW_VERSION": "3.1.2",
+                            "HDF5_VERSION": "1.8.21",
+                            "LOG4CPLUS_VERSION": "1.1.2",
+                            "NINJA_VERSION": "1.10.0",
+                            "NUMPY_VERSION": "1.16",
+                            "OCIO_CONFIGS_VERSION": "1.0_r2",
+                            "OCIO_VERSION": "1.1.1",
+                            "OIIO_VERSION": "2.1.13.0",
+                            "OPENEXR_VERSION": "2.4.0",
+                            "OPENSUBDIV_VERSION": "3_4_3",
+                            "OPENVDB_VERSION": "7.1.0",
+                            "OSL_VERSION": "1.10.10",
+                            "OTIO_VERSION": "0.12.1",
+                            "PARTIO_VERSION": "1.10.1",
+                            "PKGS_COMMON_CMAKE_VERSION": "3.12.4",
+                            "PTEX_VERSION": "2.3.2",
+                            "PYSIDE_VERSION": "5.12.6",
+                            "PYTHON_VERSION": "3.7.3",
+                            "QT_VERSION": "5.12.6",
+                            "SONAR_VERSION": "3.3.0.1492",
+                            "TBB_VERSION": "2019_U6",
+                            "USD_VERSION": "20.11",
                             "VFXPLATFORM_VERSION": "2020",
                         },
                         "labels": {
@@ -208,14 +224,43 @@ class TestBuilder(unittest.TestCase):
                         "context": ".",
                         "dockerfile": "ci-base/Dockerfile",
                         "args": {
+                            "ALEMBIC_VERSION": "1.7.11",
                             "ASWF_ORG": "aswflocaltesting",
                             "ASWF_PKG_ORG": "aswftesting",
                             "ASWF_VERSION": base_versions[1],
+                            "BLOSC_VERSION": "1.5.0",
+                            "BOOST_VERSION": "1.66.0",
+                            "CCACHE_VERSION": "4.0",
                             "CI_COMMON_VERSION": "1",
                             "CLANG_MAJOR_VERSION": "7",
+                            "CLANG_VERSION": "7.1.0",
+                            "CMAKE_VERSION": "3.12.4",
+                            "CPPUNIT_VERSION": "1.14.0",
                             "CUDA_VERSION": "10.2",
                             "DTS_VERSION": "6",
-                            "PYTHON_VERSION": "2.7",
+                            "GLEW_VERSION": "2.1.0",
+                            "GLFW_VERSION": "3.1.2",
+                            "HDF5_VERSION": "1.8.21",
+                            "LOG4CPLUS_VERSION": "1.1.2",
+                            "NINJA_VERSION": "1.10.0",
+                            "NUMPY_VERSION": "1.14",
+                            "OCIO_CONFIGS_VERSION": "1.0_r2",
+                            "OCIO_VERSION": "1.1.0",
+                            "OIIO_VERSION": "2.0.8",
+                            "OPENEXR_VERSION": "2.3.0",
+                            "OPENSUBDIV_VERSION": "3_3_3",
+                            "OPENVDB_VERSION": "6.2.1",
+                            "OSL_VERSION": "1.10.9",
+                            "OTIO_VERSION": "0.12.1",
+                            "PARTIO_VERSION": "1.10.1",
+                            "PKGS_COMMON_CMAKE_VERSION": "3.12.4",
+                            "PTEX_VERSION": "2.1.33",
+                            "PYSIDE_VERSION": "5.12.6",
+                            "PYTHON_VERSION": "2.7.15",
+                            "QT_VERSION": "5.12.6",
+                            "SONAR_VERSION": "3.3.0.1492",
+                            "TBB_VERSION": "2018",
+                            "USD_VERSION": "19.11",
                             "VFXPLATFORM_VERSION": "2019",
                         },
                         "labels": {
