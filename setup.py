e579a8279f31fd21613bab6fd3ad1340056863cc
Aloys Baillet <aloys.baillet+github@gmail.com>
2020-11-07 23:14:31 +1100

    New patch and added missing packages
    Signed-off-by: Aloys Baillet <aloys.baillet+github@gmail.com>

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("python/README.md", "r") as fh:
 
 setup(
     name="aswfdocker",
-    version="0.3.1",
+    version="0.3.2",
     author="Aloys Baillet",
     author_email="aloys.baillet+github@gmail.com",
     description="ASWF Docker Utilities",
@@ -29,6 +29,7 @@ setup(
         "click==7.1.2",
         "deprecated==1.2.10; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3'",
         "idna==2.9; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3'",
+        "importlib-resources==3.3.0",
         "jinja2==2.11.2",
         "markupsafe==1.1.1; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3'",
         "pygithub==1.51",
@@ -37,6 +38,7 @@ setup(
         "requests==2.23.0",
         "urllib3==1.25.9; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3, 3.4' and python_version < '4'",
         "wrapt==1.12.1",
+        "zipp==3.4.0; python_version < '3.8'",
     ],
     python_requires=">=3.6",
     entry_points={"console_scripts": ["aswfdocker=aswfdocker.cli.aswfdocker:cli",],},
