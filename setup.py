da4e826f0109197fdfceae34e520572d3dc4f2e3
Aloys Baillet <aloys.baillet+github@gmail.com>
2020-11-07 22:47:27 +1100

    Fixed setup.py for pypi use
    Signed-off-by: Aloys Baillet <aloys.baillet+github@gmail.com>

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache 2 License",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
     install_requires=[
@@ -38,6 +38,6 @@ setup(
         "urllib3==1.25.9; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3, 3.4' and python_version < '4'",
         "wrapt==1.12.1",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.8",
     entry_points={"console_scripts": ["aswfdocker=aswfdocker.cli.aswfdocker:cli",],},
 )
