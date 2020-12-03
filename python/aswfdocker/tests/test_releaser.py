64bc7fc3129d4feeb6271b721ea4ea6ed07cf996
Simran Spiller <simran.spiller@gmail.com>
2020-12-03 12:06:26 +0100

    Capitalization, spelling
    Signed-off-by: Simran Spiller <simran.spiller@gmail.com>

--- python/aswfdocker/tests/test_releaser.py
+++ python/aswfdocker/tests/test_releaser.py
@@ -51,7 +51,7 @@ class TestReleaser(unittest.TestCase):
             draft=False,
             object=utils.get_current_sha(),
             prerelease=False,
-            release_message=f"Inspect released docker image here: https://hub.docker.com/r/aswflocaltesting/ci-package-boost/tags?name={boost_version}",
+            release_message=f"Inspect released Docker image here: https://hub.docker.com/r/aswflocaltesting/ci-package-boost/tags?name={boost_version}",
             release_name=f"aswflocaltesting/ci-package-boost:{boost_version}",
             tag_message=f"aswflocaltesting/ci-package-boost:{boost_version}",
             type="commit",
