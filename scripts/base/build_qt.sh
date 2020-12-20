0b9d818a850fb2da73f5d3a0900f40720c6aa71d
Aloys Baillet <aloys.baillet+github@gmail.com>
2020-12-20 22:50:49 +1100

    With backward compatibility with older Qt versions
    Signed-off-by: Aloys Baillet <aloys.baillet+github@gmail.com>

--- scripts/base/build_qt.sh
+++ scripts/base/build_qt.sh
@@ -18,6 +18,7 @@ if [[ $ASWF_QT_VERSION == 5.6.1 ]]; then
     mv qt-adsk-5.6.1-vfx qt-src
     cd qt-src
     tar xf ../qt561-webkit.tgz
+    EXTRA_CONFIGURE_ARGS=-qt-xcb
 else
     QT_MAJOR_MINOR=$(echo "${ASWF_QT_VERSION}" | cut -d. -f-2)
     if [ ! -f "$DOWNLOADS_DIR/qt-${ASWF_QT_VERSION}.tar.xz" ]; then
@@ -26,28 +27,9 @@ else
     tar xf "$DOWNLOADS_DIR/qt-${ASWF_QT_VERSION}.tar.xz"
     mv "qt-everywhere-src-${ASWF_QT_VERSION}" qt-src
     cd qt-src
-    
-fi
-
-./configure \
-    --prefix="${ASWF_INSTALL_PREFIX}" \
-        -no-strip \
-        -no-rpath \
-        -opensource \
-        -plugin-sql-sqlite \
-        -verbose \
-        -opengl desktop \
+    if [[ $QT_MAJOR_MINOR == 5.15 ]]; then
+        EXTRA_CONFIGURE_ARGS="-no-sql-mysql \
         -xcb \
-        -no-warnings-are-errors \
-        -no-libudev \
-        -no-egl \
-        -nomake examples \
-        -nomake tests \
-        -c++std c++14 \
-        -confirm-license \
-        -no-use-gold-linker \
-        -release \
-        -no-sql-mysql \
         -qt-libjpeg \
         -qt-libpng \
         -bundled-xcb-xinput \
@@ -57,7 +39,32 @@ fi
         -R . \
         -icu \
         -skip qtnetworkauth \
-        -skip qtpurchasing
+        -skip qtpurchasing \
+        -I /usr/include/openssl11 -L /usr/lib64/openssl11"
+    else
+        EXTRA_CONFIGURE_ARGS=-qt-xcb
+    fi
+fi
+
+./configure \
+    --prefix="${ASWF_INSTALL_PREFIX}" \
+        -no-strip \
+        -no-rpath \
+        -opensource \
+        -plugin-sql-sqlite \
+        -openssl \
+        -verbose \
+        -opengl desktop \
+        -no-warnings-are-errors \
+        -no-libudev \
+        -no-egl \
+        -nomake examples \
+        -nomake tests \
+        -c++std c++14 \
+        -confirm-license \
+        -no-use-gold-linker \
+        -release \
+        ${EXTRA_CONFIGURE_ARGS}
 
 make -j$(nproc)
 
