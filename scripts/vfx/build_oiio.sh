2756ac3ee3066baaba2c29cd76b2e29efa259947
Aloys Baillet <aloys.baillet+github@gmail.com>
2021-09-04 17:46:05 +1000

    Updated to new oiio tag format
    Signed-off-by: Aloys Baillet <aloys.baillet+github@gmail.com>

--- scripts/vfx/build_oiio.sh
+++ scripts/vfx/build_oiio.sh
@@ -7,8 +7,14 @@ set -ex
 git clone https://github.com/OpenImageIO/oiio.git
 cd oiio
 
+if [[ $ASWF_OIIO_VERSION == 2.0* || $ASWF_OIIO_VERSION == 2.1* || $ASWF_OIIO_VERSION == 2.2*  ]]; then
+    OIIO_TAG="Release-${ASWF_OIIO_VERSION}"
+else
+    OIIO_TAG="v${ASWF_OIIO_VERSION}"
+fi
+
 if [ "$ASWF_OIIO_VERSION" != "latest" ]; then
-    git checkout "tags/Release-${ASWF_OIIO_VERSION}" -b "Release-${ASWF_OIIO_VERSION}"
+    git checkout "tags/$OIIO_TAG" -b $OIIO_TAG
 fi
 
 mkdir build
