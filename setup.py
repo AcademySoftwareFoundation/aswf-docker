51e7c23e6c657ce6f0b2ec62b8b5c0e16033ce7f
Aloys Baillet <aloys.baillet+github@gmail.com>
2022-05-26 09:51:42 +1000

    Updated aswfdocker version
    Signed-off-by: Aloys Baillet <aloys.baillet+github@gmail.com>

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("python/README.md", "r") as fh:
 
 setup(
     name="aswfdocker",
-    version="0.6.0",
+    version="0.7.0",
     author="Aloys Baillet",
     author_email="aloys.baillet+github@gmail.com",
     description="ASWF Docker Utilities",
