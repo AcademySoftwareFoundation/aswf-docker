cd37e4f01bd5267172a7e02b62947ca40e5cca94
Aloys Baillet <aloys.baillet+github@gmail.com>
2022-12-18 22:10:05 +1100

    Fixed rsync regression
    Signed-off-by: Aloys Baillet <aloys.baillet+github@gmail.com>

--- scripts/common/copy_new_files.sh
+++ scripts/common/copy_new_files.sh
@@ -8,4 +8,4 @@ mkdir -p /package
 
 cd "${ASWF_INSTALL_PREFIX}"
 find . -type l -o -type f | cut -c3- > /tmp/new-prefix-files.txt
-rsync -av --files-from=/tmp/new-prefix-files.txt --exclude-from=/tmp/previous-prefix-files.txt . /package/
+rsync -av --trust-sender --files-from=/tmp/new-prefix-files.txt --exclude-from=/tmp/previous-prefix-files.txt . /package/
