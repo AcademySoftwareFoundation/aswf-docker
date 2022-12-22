706838b35cd49135cbb02a3ad6517bb49059d459
Aloys Baillet <aloys.baillet+github@gmail.com>
2022-12-22 11:38:07 +1100

    restore old rsync command
    Signed-off-by: Aloys Baillet <aloys.baillet+github@gmail.com>

--- scripts/common/copy_new_files.sh
+++ scripts/common/copy_new_files.sh
@@ -8,4 +8,4 @@ mkdir -p /package
 
 cd "${ASWF_INSTALL_PREFIX}"
 find . -type l -o -type f | cut -c3- > /tmp/new-prefix-files.txt
-RSYNC_OLD_ARGS=1 rsync -av --trust-sender --files-from=/tmp/new-prefix-files.txt --exclude-from=/tmp/previous-prefix-files.txt . /package/
+rsync -av --files-from=/tmp/new-prefix-files.txt --exclude-from=/tmp/previous-prefix-files.txt . /package/
