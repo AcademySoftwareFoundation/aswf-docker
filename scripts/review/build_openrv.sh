2398b9f279b2889a4856511b78b137009ada7c54
Cédrik Fuoco <cedrik.fuoco@autodesk.com>
2024-09-12 09:07:28 -0400

    Update expat to 2.6.3 and little improvement for build_openrv.sh
    Signed-off-by: Cédrik Fuoco <cedrik.fuoco@autodesk.com>

--- scripts/review/build_openrv.sh
+++ scripts/review/build_openrv.sh
@@ -15,14 +15,8 @@ git clone --recursive https://github.com/AcademySoftwareFoundation/OpenRV.git .
 # causes issues during the build when Qt is within system folder (e.g. /usr/local)
 export QT_HOME=/tmp/qttemp
 
-# Expand aliases.
-shopt -s expand_aliases
-
-# Setup environments for OpenRV.
-source rvcmds.sh
-
-# Execute the rvsetup command to install Python dependencies.
-rvsetup
+#Install Python dependencies. Do not rely on rvcmds.sh.
+python3 -m pip install --upgrade -r ./requirements.txt
 
 # There are decrepencies between Qt6 from Conan and Qt6 from the online installer regardings some directories location:
 # - libexec directory is in the bin directory
@@ -30,18 +24,18 @@ rvsetup
 # - resources is under res/datadir
 
 # Copy those folder to the same locations as the official online installer because OpenRV expected them to be there.
-mkdir /tmp/qttemp/libexec
-mkdir /tmp/qttemp/translations
-mkdir /tmp/qttemp/resources
-cp -R /tmp/qttemp/bin/* /tmp/qttemp/libexec
-cp -R /tmp/qttemp/res/datadir/translations/* /tmp/qttemp/translations
-cp -R /tmp/qttemp/res/datadir/resources/* /tmp/qttemp/resources
+mkdir $QT_HOME/libexec
+mkdir $QT_HOME/translations
+mkdir $QT_HOME/resources
+cp -R $QT_HOME/bin/* $QT_HOME/libexec
+cp -R $QT_HOME/res/datadir/translations/* $QT_HOME/translations
+cp -R $QT_HOME/res/datadir/resources/* $QT_HOME/resources
 
 # Configure setp.
 cmake -B _build \
       -G Ninja \
       -DCMAKE_BUILD_TYPE=Release \
-      -DRV_DEPS_QT6_LOCATION=/tmp/qttemp \
+      -DRV_DEPS_QT6_LOCATION=$QT_HOME \
       -DRV_VFX_PLATFORM=CY2024
 
 # Build dependencies.
